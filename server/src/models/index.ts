f17b0ac735c155900e5fc41c0c43d7df9d3bbbbb
Bugatti1982 <welch8119@gmail.com>
2024-11-12 19:27:16 -0600

    New Files

---
+++ server/src/models/index.ts
@@ -0,0 +1,13 @@
+import sequelize from '../config/connection.js';
+
+import { UserFactory } from './user.js';
+import { TicketFactory } from './ticket.js';
+
+
+const User = UserFactory(sequelize);
+const Ticket = TicketFactory(sequelize);
+
+User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
+Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
+
+export { User, Ticket };
